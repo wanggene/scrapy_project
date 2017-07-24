cf234040ae43691365117be29c0cc6c534e75003
wanggene <onedna@gmail.com>
2017-07-24 17:29:43 -0400

    update detailed page

--- realtor/realtor/spiders/realtor_spider.py
+++ realtor/realtor/spiders/realtor_spider.py
@@ -48,9 +48,7 @@ class RealtorSpider(Spider):
 			lotunit = rows[i].xpath('./div[1]/div[2]/div/div[6]/ul/li[4]/text()').extract_first()
 			detailslink= rows[i].xpath('./div[1]/div[2]/div/div/a/@href').extract_first()
 
-			if detailslink:
-				details_url = response.urljoin(detailslink)
-				yield Request(url = details_url, callback = self.parse_details)
+
 
 
 			# verify 
@@ -70,43 +68,13 @@ class RealtorSpider(Spider):
 			detailslink = self.verify(detailslink)
 
 
-# details
-			percent_Nearby = self.verify(percent_Nearby)
-			trend_Nearby = self.verify(trend_Nearby)
-			days_onmarket = self.verify(days_onmarket)
-			onmarket = self.verify(onmarket)
-			price_change = self.verify(price_change)
-			price_trend = self.verify(price_trend)
-
-
-
-# item
-			item = RealtorItem()
-			item['propertyid'] = propertyid
-			item['address'] = address
-			item['city'] = city
-			item['soldPrice'] = soldPrice
-			item['soldDate'] = soldDate
-			item['state'] = state
-			item['zipcode'] = zipcode
-			item['propertyType'] = propertyType
-			item['bedroom'] = bedroom
-			item['bathroom'] = bathroom
-			item['floorsize'] = floorsize
-			item['lotsize'] = lotsize
-			item['lotunit'] = lotunit
-			item['detailslink'] = detailslink
-
-			item['percent_Nearby'] = percent_Nearby
-			item['trend_Nearby'] = trend_Nearby
-			item['days_onmarket'] = days_onmarket
-			item['onmarket'] = onmarket
-			item['price_change'] = price_change
-			item['price_trend'] = price_trend
-
-
-			yield item
-
+			if detailslink:
+				details_url = response.urljoin(detailslink)
+				yield Request(url = details_url, callback = self.parse_details, 
+					meta={'propertyid': propertyid, 'address': address, 'city':city, 'soldPrice': soldPrice,
+					'soldDate': soldDate, 'state' : state, 'zipcode': zipcode, 'propertyType' :propertyType,
+					'bedroom': bedroom, 'bathroom': bathroom, 'floorsize':floorsize,'lotsize' :lotsize,
+					'lotunit': lotunit, 'detailslink': detailslink })
 
 
 		# following pagination link
@@ -118,6 +86,21 @@ class RealtorSpider(Spider):
 
 
 	def parse_details(self, response):
+		propertyid = response.meta['propertyid']
+		address = response.meta['address']
+		city = response.meta['city']
+		soldPrice = response.meta['soldPrice']
+		soldDate = response.meta['soldDate']
+		state = response.meta['state']
+		zipcode = response.meta['zipcode']
+		propertyType = response.meta['propertyType']
+		bedroom = response.meta['bedroom']
+		bathroom = response.meta['bathroom']
+		floorsize = response.meta['floorsize']
+		lotsize = response.meta['lotsize']
+		lotunit = response.meta['lotunit']
+		detailslink = response.meta['detailslink']
+
 		details = response.xpath('//*[@id="market-summary-data"]')
 
 		percent_Nearby = details.xpath('./div/div[1]/div/div/div[2]/div/text()').extract_first()
@@ -129,9 +112,44 @@ class RealtorSpider(Spider):
 		price_change = details.xpath('./div/div[3]/div/div/div[2]/div/text()').extract_first()
 		price_trend = details.xpath('./div/div[3]/div/div/div[2]/span/text()').extract_first()
 
-		# verify details
-		location = esponse.xpath('//*[@id="ldp-amenities-map-container"]')
-		lat = location.xpath('./div/@data-property-lat').extract_first()
+		# lat 
+		#location = esponse.xpath('//*[@id="ldp-amenities-map-container"]')
+		#lat = location.xpath('./div/@data-property-lat').extract_first()
 
+
+# details
+		percent_Nearby = self.verify(percent_Nearby)
+		trend_Nearby = self.verify(trend_Nearby)
+		days_onmarket = self.verify(days_onmarket)
+		onmarket = self.verify(onmarket)
+		price_change = self.verify(price_change)
+		price_trend = self.verify(price_trend)
+
+# item
+		item = RealtorItem()
+		item['propertyid'] = propertyid
+		item['address'] = address
+		item['city'] = city
+		item['soldPrice'] = soldPrice
+		item['soldDate'] = soldDate
+		item['state'] = state
+		item['zipcode'] = zipcode
+		item['propertyType'] = propertyType
+		item['bedroom'] = bedroom
+		item['bathroom'] = bathroom
+		item['floorsize'] = floorsize
+		item['lotsize'] = lotsize
+		item['lotunit'] = lotunit
+		item['detailslink'] = detailslink
+
+		item['percent_Nearby'] = percent_Nearby
+		item['trend_Nearby'] = trend_Nearby
+		item['days_onmarket'] = days_onmarket
+		item['onmarket'] = onmarket
+		item['price_change'] = price_change
+		item['price_trend'] = price_trend
+
+
+		yield item
 
 
